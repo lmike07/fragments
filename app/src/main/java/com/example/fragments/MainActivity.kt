8bed136794609d2a3c392e3a7e72c1fc4c6a1462
Mayconlucas <lmike.infos@gmail.com>
2024-09-18 20:50:26 -0300

    Fragments

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.fragments
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.fragments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.botao1.setOnClickListener {
+            irPraFragment(Fragment1())
+        }
+        binding.botao2.setOnClickListener {
+            irPraFragment(Fragment2())
+        }
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    private fun irPraFragment(fragment:Fragment) {
+        fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.fragments, fragment).commit()
+    }
+}
